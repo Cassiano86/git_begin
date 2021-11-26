a734c44f3afbbf33dae731835c3cec9ccf63374e
Cassiano86 <cassiano_tst@hotmail.com>
2021-11-26 17:54:11 -0300

    Relacionamentos de pessoa e perfil criado

---
+++ app/Http/Controllers/PerfilModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PerfilModel;
+use Illuminate\Http\Request;
+
+class PerfilModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PerfilModel  $perfilModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PerfilModel $perfilModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PerfilModel  $perfilModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PerfilModel $perfilModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PerfilModel  $perfilModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PerfilModel $perfilModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PerfilModel  $perfilModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PerfilModel $perfilModel)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PessoaModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PessoaModel;
+use Illuminate\Http\Request;
+
+class PessoaModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PessoaModel  $pessoaModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PessoaModel $pessoaModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PessoaModel  $pessoaModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PessoaModel $pessoaModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PessoaModel  $pessoaModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PessoaModel $pessoaModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PessoaModel  $pessoaModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PessoaModel $pessoaModel)
+    {
+        //
+    }
+}

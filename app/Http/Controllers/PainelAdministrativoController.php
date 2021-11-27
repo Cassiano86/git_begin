42a8b9cc7d06e6ea80e72e1337d8f4f083517e26
Cassiano86 <cassiano_tst@hotmail.com>
2021-11-27 16:14:43 -0300

    Criando Painel Administrativo Controller

---
+++ app/Http/Controllers/PainelAdministrativoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PainelAdministrativoController extends Controller
+{
+    public function index(){
+        dd('Bem vindo');
+    }
+}
